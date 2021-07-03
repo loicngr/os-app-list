a206cad88a9b3af47012522b474f387d63566119
loicngr <pro.nogierloic@gmail.com>
2021-07-03 19:45:45 +0200

    Add consts

--- src/common/consts.rs
+++ src/common/consts.rs
@@ -3,3 +3,5 @@ pub const WINDOWS_TYPE: u8 = 200;
 pub const WINDOWS_KEY_STR: &'static str = "windows";
 pub const MACOS_KEY_STR: &'static str = "macos";
 pub const ERROR_COMMAND_OUTPUT_PANIC: &'static str = "Error";
+pub const ERROR_CHECK_OS: &'static str = "Error when match os type";
+pub const ERROR_CHECK_FOLDER_NOT_EXIST: &'static str = "Error folder not exist";
